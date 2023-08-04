9ec01c66fa4185846b543861ca04543e7396a5b2
Fuad208 <127172658+Fuad208@users.noreply.github.com>
2023-08-04 21:09:20 +0700

    Add files via upload

---
+++ FUAD060423/BukuAlamat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FUAD060423;
+
+/**
+ *
+ */
+public class BukuAlamat {
+   public static void main (String[] args){
+        BukuAlamatData BukuAlamat1 = new BukuAlamatData("Fayy","Solok",
+        "082321210404","fay21@gmail.com");
+        BukuAlamat1.print("");
+    }
+
+}
---
+++ FUAD060423/StudentRecordExam.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FUAD060423;
+
+/**
+ *
+ */
+public class StudentRecordExam {
+    public static void main(String[] args){
+        StudentRecord anna = new StudentRecord("Anna","Padang");
+        anna.print("");
+        
+        System.out.println("Jumlah Record :"+StudentRecord.getStudentCount());
+    }
+    
+}
