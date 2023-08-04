18a099b67f56def5a7f3d3323fac6dd3ddb08c35
Fuad208 <127172658+Fuad208@users.noreply.github.com>
2023-08-04 18:51:06 +0700

    Add files via upload

---
+++ FUAD300323/ContohCasting.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FUAD300323;
+
+/**
+ *
+ */
+public class ContohCasting {
+    public static void main(String[] args){
+        //char valChar = 'A';
+        char valChar = 'a';
+        int valInt = valChar;
+        
+        double valDouble = 10.12;
+        
+        double x = 10.12;
+        int    y = 2;
+        
+        int result = (int)(x/y); //hasil typecast operasi ke int
+        
+        System.out.print( valInt); //casting explist : keluaran 65
+        
+    }
+}
