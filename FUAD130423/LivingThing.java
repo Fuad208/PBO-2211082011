f523318541a56e4db94de9c0f6134c9dc2c639ea
Fuad208 <127172658+Fuad208@users.noreply.github.com>
2023-08-04 21:28:35 +0700

    Add files via upload

---
+++ FUAD130423/LivingThing.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FUAD130423;
+
+/**
+ *
+ */
+public abstract class LivingThing {
+    public void breath(){ 
+        System.out.println("Living Thing breathing..."); 
+    } 
+    
+    public void eat(){ 
+        System.out.println("Living Thing eating..."); 
+    } 
+    
+    /** 
+    * abstract method walk 
+    * Kita ingin method ini di-overridden oleh subclasses 
+    */ 
+    
+    public abstract void walk();
+    
+    public class Human extends LivingThing 
+    { 
+        public void walk(){ 
+            System.out.println("Human walks...");
+        } 
+    } 
+}
