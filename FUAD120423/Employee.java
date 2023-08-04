97197386b620efabd4007c05cb64d0e0ae5efd0b
Fuad208 <127172658+Fuad208@users.noreply.github.com>
2023-08-04 21:19:54 +0700

    Add files via upload

---
+++ FUAD120423/Employee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FUAD120423;
+
+/**
+ *
+ */
+public class Employee extends Person {
+    @Override
+    public String getName(){
+        System.out.println("getName Employee");
+        return name;
+    }
+}
---
+++ FUAD120423/StudentExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FUAD120423;
+
+/**
+ *
+ */
+public class StudentExample {
+    public static void main(String[] args){
+        Student student = new Student();
+        System.out.println("Nama    : "+student.getName());
+        System.out.println("Alamat  : "+student.getAddress());
+    }
+}
